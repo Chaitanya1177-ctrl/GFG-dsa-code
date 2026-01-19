ee542bf90b0882bed614966d19c0b2dcd13b0772
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-19 19:48:46 +0400

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove Spaces/remove-spaces.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+
+    String modify(String s) {
+        // your code here
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!=' ')
+            {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
