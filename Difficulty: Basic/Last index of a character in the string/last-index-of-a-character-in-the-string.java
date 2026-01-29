e063d1c96bac6e2c84c7de8867fa6335b4d794c1
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-01-29 16:16:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Last index of a character in the string/last-index-of-a-character-in-the-string.java
+++ Difficulty: Basic/Last index of a character in the string/last-index-of-a-character-in-the-string.java
@@ -4,7 +4,9 @@ class Solution {
     int LastIndex(String s, char p) {
         // code here
         for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)==p)return i;
+            if(s.charAt(i)==p){
+                return i;
+            }
         }
         return -1;
     }
