cd3a69674358b3f5cb7f55a60954b622f66efb50
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-01 15:43:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subarrays with equal 1s and 0s/subarrays-with-equal-1s-and-0s.java
+++ Difficulty: Medium/Subarrays with equal 1s and 0s/subarrays-with-equal-1s-and-0s.java
@@ -3,18 +3,21 @@ class Solution {
         // code here
         int n=arr.length;
         int p[]=new int[n];
-        p[0]=((arr[0]==0)?-1:1);
         int count=0;
-        HashMap<Integer,Integer>ss=new HashMap<>();
-        
-        ss.put(0,1);
+        p[0]=((arr[0]==0)?-1:1);
         for(int i=1;i<n;i++){
             p[i]=p[i-1]+((arr[i]==0)?-1:1);
         }
+        HashMap<Integer,Integer>ss=new HashMap<>();
+        ss.put(0,1);
         for(int i=0;i<n;i++){
-            if(ss.containsKey(p[i]))count+=ss.get(p[i]);
+            // int val=p[i]-k;
+            if(ss.containsKey(p[i])){
+                count+=ss.get(p[i]);
+            }
             ss.put(p[i],ss.getOrDefault(p[i],0)+1);
         }
-        return count;
-    }
+        
+    return count;
+}
 }
