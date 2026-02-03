0f1107a9ad8f5ccec0d2b24a4150caf46fa89925
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-03 22:10:26 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
+++ Difficulty: Easy/String Duplicates Removal/string-duplicates-removal.java
@@ -1,11 +1,13 @@
+// User function Template for Java
+
 class Solution {
     String removeDuplicates(String s) {
-        StringBuilder sb = new StringBuilder();
-        HashSet<Character> set = new HashSet<>();
-
-        for (char c : s.toCharArray()) {
-            if (!set.contains(c)) {
-                set.add(c);
+        StringBuilder sb=new StringBuilder();
+        HashSet<Character>ss=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(!ss.contains(c)){
+                ss.add(c);
                 sb.append(c);
             }
         }
