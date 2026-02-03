09013f2700feecbe9a53ffeca0fcc2cb2f675129
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-03 21:59:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.java
@@ -3,16 +3,17 @@
 class Solution {
     String removeVowels(String s) {
         // code here
-        StringBuilder sb =new StringBuilder();
-        for(int i=0;i<s.length();i++ ){
-            char ch =s.charAt(i);	if (ch != 'a' && ch != 'e' && ch != 'i' &&
+        
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            
+               char ch =s.charAt(i);	
+               if (ch != 'a' && ch != 'e' && ch != 'i' &&
                 ch != 'o' && ch != 'u' &&
                 ch != 'A' && ch != 'E' && ch != 'I' &&
-                ch != 'O' && ch != 'U') {
+                ch != 'O' && ch != 'U'){
                     sb.append(ch);
                 }
-            
-            
         }
         return sb.toString();
     }
