555f23bb4dfd14c7520e3012f20f7842881949b5
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-04 22:45:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Anagram/anagram.java
+++ Difficulty: Easy/Anagram/anagram.java
@@ -2,18 +2,17 @@ class Solution {
     public static boolean areAnagrams(String s1, String s2) {
         // code here
         if(s1.length()!=s2.length())return false;
-    HashMap<Character,Integer>ss=new HashMap<>();
-    for(char c1:s1.toCharArray()){
-        ss.put(c1,ss.getOrDefault(c1,0)+1);
-    }
-    for(char c2:s2.toCharArray()){
-        ss.put(c2,ss.getOrDefault(c2,0)-1);
-    }
-    for(int val:ss.values()){
-        if(val!=0){
-            return false;
+        HashMap<Character,Integer>ss=new HashMap<>();
+        for(char c:s1.toCharArray
+        ()){
+            ss.put(c,ss.getOrDefault(c,0)+1);
         }
-    }
-    return true;
+        for(char c:s2.toCharArray()){
+            ss.put(c,ss.getOrDefault(c,0)-1);
+        }
+        for(int val:ss.values()){
+            if(val!=0)return false;
+        }
+        return true;
     }
 }
