d36e8af92de85a3352c56ed7c3d4d1626809b628
Chaitanya1177-ctrl <chaitanyapatil1177@gmail.com>
2026-02-05 16:30:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Identical Trees/identical-trees.java
+++ Difficulty: Easy/Identical Trees/identical-trees.java
@@ -12,9 +12,9 @@ class Node{
 class Solution {
     public boolean isIdentical(Node r1, Node r2) {
         // code here
-        if(r1==null && r2==null)return true;
-        if(r1==null   || r2==null   )return false;
-        return (r1.data==r2.data) && isIdentical(r1.left,r2.left) && isIdentical(r1.right,r2.right);
         
+        if(r1==null && r2==null)return true;
+        if(r1==null   || r2==null)return false;
+        return (r1.data==r2.data) && isIdentical (r1.left,r2.left) && isIdentical (r1.right,r2.right);
     }
 }
