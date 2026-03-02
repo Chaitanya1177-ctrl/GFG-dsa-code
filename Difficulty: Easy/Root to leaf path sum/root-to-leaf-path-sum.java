d0f8e2e603200b70549ebfd89797cce4c528416c
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-02 13:18:59 +0400

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Root to leaf path sum/root-to-leaf-path-sum.java
+++ Difficulty: Easy/Root to leaf path sum/root-to-leaf-path-sum.java
@@ -17,12 +17,8 @@ class Solution {
     boolean hasPathSum(Node root, int target) {
         // Your code here
         if(root==null)return false;
-        if(root.left==null && root.right==null){
-            return target==root.data;
-        }
+        if(root.left==null && root.right==null)return target==root.data;
         int rem=target-root.data;
-        
-        return hasPathSum(root.left,rem)||hasPathSum(root.right,rem);
-        
+        return hasPathSum(root.left,rem)|| hasPathSum(root.right,rem);
     }
 }
