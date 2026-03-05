da7d35f254d68d2e2c698c86f4b9e116768fab1d
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-05 12:03:06 +0400

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check if array contains duplicates/check-if-array-contains-duplicates.java
+++ Difficulty: Easy/Check if array contains duplicates/check-if-array-contains-duplicates.java
@@ -3,9 +3,9 @@
 class Solution {
     public boolean checkDuplicates(int arr[]) {
         // Code here
-        Arrays.sort(arr);
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]==arr[i-1]){
+        HashSet<Integer>ss=new HashSet<>();
+        for(int a:arr){
+            if(!ss.add(a)){
                 return true;
             }
         }
