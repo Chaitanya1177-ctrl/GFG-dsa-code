073ea21548c328582a982fba507cb548735ea774
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-12 09:06:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Previous Greater Element/previous-greater-element.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> preGreaterEle(int[] arr) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for(int i = 0; i < arr.length; i++){
+
+            while(!st.isEmpty() && st.peek() <= arr[i]){
+                st.pop();
+            }
+
+            if(st.isEmpty()){
+                ans.add(-1);
+            }else{
+                ans.add(st.peek());
+            }
+
+            st.push(arr[i]);
+        }
+
+        return ans;
+    }
+}
