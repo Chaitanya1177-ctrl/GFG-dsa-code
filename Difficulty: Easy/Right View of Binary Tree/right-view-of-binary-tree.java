c5df65d0efc3219eff23dfbec82ae35b79e1e3af
chaitanya patil <chaitanyapatil1177@gmail.com>
2026-03-21 09:30:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Right View of Binary Tree/right-view-of-binary-tree.java
@@ -0,0 +1,29 @@
+/*
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+*/
+
+class Solution {
+    public ArrayList<Integer> rightView(Node root) {
+        // code here
+        ArrayList<Integer>ss=new ArrayList<>();
+        ss(root,0,ss);
+        return ss;
+    }
+    void ss(Node root,int level,ArrayList<Integer>ans){
+        if(root==null)return ;
+        if(level==ans.size()){
+            ans.add(root.data);
+        }
+        ss(root.right,level+1,ans);
+        ss(root.left,level+1,ans);
+    }
+}
